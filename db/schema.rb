ddc937794c11c533699b2b71e706e2d73ea02bb4
Sarah Price <sarah.e.price@gmail.com>
2015-09-05 18:42:34 +0000

    Added association for plans and users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150905182801) do
+ActiveRecord::Schema.define(version: 20150905183921) do
 
   create_table "contacts", force: true do |t|
     t.string   "name"
